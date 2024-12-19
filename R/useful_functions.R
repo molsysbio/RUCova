363cb02b22ddf0e83f2c79807996357ae152c612
Rosario Astaburuaga-García <rosario.astaburuaga@charite.de>
2024-12-19 16:27:19 +0100

    interact with SingleCellExperiment

--- R/useful_functions.R
+++ R/useful_functions.R
@@ -1,11 +1,9 @@
 #' Calculated mean of normalised Iridium isotopes
 #'
-#' @param sce SingleCellExperiment object with counts in linear scale 
+#' @param data SingleCellExperiment object with counts in linear scale 
 #' @param dna_channels Vector specifying the names of the DNA channels
 #' @param q Quantile for normalisation.
-#' @return A vector.
-#' @examples 
-#'
+#' @return The SingleCellExperiment object with an extra column "mean_BC" in the counts assay.#'
 #' @export
 #' @import dplyr
 #' @import ComplexHeatmap
@@ -52,15 +50,11 @@ calc_mean_DNA <- function(data,dna_channels, q) {
 
 #' Calculated mean of normalised highest BC per cell
 #'
-#' @param sce SingleCellExperiment object with counts in linear scale 
-#' @param dna_channels Vector specifying the names of the DNA channels
+#' @param data SingleCellExperiment object with counts in linear scale 
+#' @param bc_channels Vector specifying the names of the BC channels
 #' @param q Quantile for normalisation.
 #' @param n_bc number of barcoding isotopes per cell.
-#' @return A vector.
-#' @examples
-#' data <- RUCova::HNSCC_data
-#' data |> dplyr::mutate(mean_BC = RUCova::calc_mean_BC(Pd102Di, Pd104Di, Pd105Di, Pd106Di, Pd108Di, Pd110Di, n_bc = 4, q = 0.95))
-#' 
+#' @return The SingleCellExperiment object with an extra column "mean_BC" in the counts assay.
 #' @export
 #'
 
@@ -108,25 +102,6 @@ calc_mean_BC <- function(data,bc_channels, n_bc, q) {
 #' @param lower Matrix with pearson correlation coefficient between markers eg.: before RUCova, to be plotted in the lower triangle.
 #' @param upper  Matrix with pearson correlation coefficient between markers eg.: after RUCova, to be plotted in the upper triangle.
 #' @return #A heatmap
-#' @examples 
-#' data <- RUCova::HNSCC_data
-#' data <- data |> dplyr::mutate(cell_id = 1:n(),
-#'                        mean_DNA = RUCova::calc_mean_DNA(DNA_191Ir, DNA_193Ir, q = 0.95),
-#'                        mean_BC = RUCova::calc_mean_BC(Pd102Di, Pd104Di, Pd105Di, Pd106Di, Pd108Di, Pd110Di,
-#'                        Dead_cells_194Pt,Dead_cells_198Pt, n_bc = 4, q = 0.95))
-#' m <- c("pH3","IdU","Cyclin_D1","Cyclin_B1", "Ki.67","pRb","pH2A.X","p.p53","p.p38","pChk2","pCDC25c","cCasp3","cPARP","pAkt","pAkt_T308","pMEK1.2","pERK1.2","pS6","p4e.BP1","pSmad1.8","pSmad2.3","pNFkB","IkBa", "CXCL1","Lamin_B1", "pStat1","pStat3", "YAP","NICD")
-#' out <- RUCova::rucova(data, markers = m, SUCs = c("mean_DNA", "mean_BC", "total_ERK", "pan_Akt"), apply_asinh_SUCs = TRUE, col_name_sample = "line", 
-#' center_SUCs = "across_samples", model = "interaction", keep_offset = TRUE)
-#' data_reg <- out$data_reg
-#' lower <-  data |> filter(line == "Cal33") |> 
-#' dplyr::mutate_at(vars(m,x), asinh) |> 
-#' dplyr::select(m,x) |> 
-#' cor(method= "pearson")
-#' upper <-  data_reg |> filter(line == "Cal33") |> #regressed data set
-#' dplyr::mutate_at(vars(m,x), asinh) |> 
-#' dplyr::select(m,x) |> 
-#' cor(method= "pearson")
-#' heatmap_compare_corr(lower,upper)
 #' @export
 #'
 #'
